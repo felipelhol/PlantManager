62965954092666595e569d2d62e370ba20da9477
felipelhol <felipelhol@gmail.com>
2021-04-21 19:11:52 -0300

    second commit

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,39 +1,68 @@
-import React, {useState} from 'react';
-import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import { 
+  SafeAreaView,
+  View, 
+  Text, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet,
+  Dimensions } from 'react-native';
+  
+import {Feather} from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
-import {Button} from '../components/Button';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/core';
+//import {Button} from '../components/Button';
 
 
 export function Welcome(){
-  const [visible, setVisible] = useState(false);
-
-  function hanldeVisibility(){
-    setVisible(true)
+  const navigation = useNavigation();
+  
+  function handleStart(){
+    navigation.navigate('UserIdentification');
   }
+  
 
   return(
     <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
       <Text style={styles.title}>
         Gerencie {'\n'}
-        suas plantas {'\n'}
-        de forma fácil
+        suas plantas de {'\n'}
+        forma fácil
       </Text>
 
-      {
-        visible &&
-        <Image source={wateringImg} style={styles.image}/>
-      }
+      
+        <Image 
+          source={wateringImg} 
+          style={styles.image}
+          resizeMode="contain" //se ajusta a tela
+        />
+      
       
 
       <Text style={styles.subtitle}>
-      Não esqueça mais de regar suas plantas. 
-      Nós cuidamos de lembrar você sempre que precisar.
+      Não esqueça mais de regar suas {'\n'}
+      plantas. Nós cuidamos de lembrar você 
+      sempre que precisar.
       </Text>
 
-     <Button title="Avançar 1" onPress={hanldeVisibility}/>
-
+      <TouchableOpacity 
+        style={styles.button} 
+        activeOpacity={0.8}
+        onPress={handleStart}
+   //{ ...rest} não precisa por não vir de um componente
+       >
+     
+        <Feather 
+          name="chevron-right" 
+          style={styles.buttonIcon}
+        />
+      
+      </TouchableOpacity>
+      </View>
 
     </SafeAreaView>
   )
@@ -42,21 +71,34 @@ export function Welcome(){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+     //usar around pra nao colar nas bordas <> space-between
+  },
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around',
+    paddingHorizontal: 20
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
-    marginTop: 38
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
-    color: colors.heading
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  
+  image:{
+    //width: 292, o device se encarrega disso- usar as dimensions pra nao ficar fixo
+    height: Dimensions.get('window').width * 0.7
   },
   button: {
     backgroundColor: colors.green,
@@ -65,15 +107,11 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     marginBottom: 10,
     height: 56,
-    //width: 56
-    paddingHorizontal:10
+    width: 56,
+    //paddingHorizontal:10
   },
-  image:{
-    width: 292,
-    height: 284
-  },
-  buttonText:{
+  buttonIcon:{
     color: colors.white,
-    fontSize: 24
+    fontSize: 32
   }
 })
